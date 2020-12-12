d8be016f65375d677bc821010ab90aa4349117ea
chanduka <ashainxsamarasinghe@gmail.com>
2020-12-12 10:38:38 +0530

    removed items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 Amplify.configure(awsExports);
